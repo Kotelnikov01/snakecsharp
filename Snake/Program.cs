a8dea939f8113ba3f48fafa5d2da3f12afa3e1f1
Alexander <AksHD@mail.ru>
2021-04-24 18:55:12 +0300

    Горизонтальные линии

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,31 +13,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(6, 8, '^');
-            p3.Draw();
-
-            Point p4 = new Point(1, 6, '%');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            numList.RemoveAt(0);
-
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> plist = new List<Point>();
-
-            plist.Add(p1);
-            plist.Add(p2);
-            plist.Add(p3);
-            plist.Add(p4);
-
+            HorizontalLine line = new HorizontalLine(5,101,8,'+');
+            line.Drow();
 
             Console.ReadLine();
         }
